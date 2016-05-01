66401df9dfa7cdcb713e9bd897d0f74da99653f9
rolaveric <rolaveric@gmail.com>
2016-05-01 19:58:31 +1000

    Fixed Travis config.
    Fixed Travis config running Chrome.

--- karma.conf.js
+++ karma.conf.js
@@ -1,53 +1,66 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['systemjs', 'jasmine'],
+var karmaConfig = {
+  basePath: '',
+  frameworks: ['systemjs', 'jasmine'],
 
-    systemjs: {
-      baseURL: '.',
-      systemPath: 'node_modules/systemjs/dist/system.src.js',
-      configFiles: ['app/config.js'],
-      imports: ['app/model/login.spec.js'],
-      config: {}
-    },
+  systemjs: {
+    baseURL: '.',
+    systemPath: 'node_modules/systemjs/dist/system.src.js',
+    configFiles: ['app/config.js'],
+    imports: ['app/model/login.spec.js'],
+    config: {}
+  },
 
-    preprocessors: {
-      'app/index.js': ['systemjs', 'coverage'],
-      'app/states.js': ['systemjs', 'coverage'],
-      'app/*/!(*spec).js': ['systemjs', 'coverage']
-    },
+  preprocessors: {
+    'app/index.js': ['systemjs', 'coverage'],
+    'app/states.js': ['systemjs', 'coverage'],
+    'app/*/!(*spec).js': ['systemjs', 'coverage']
+  },
 
-    // list of files / patterns to load in the browser
-    files: [
-      { pattern: 'node_modules/es6-shim/es6-shim.min.js', watched: false },
-      { pattern: 'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js', watched: false },
-      { pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', watched: false },
-      { pattern: 'app/*.js', included: false },
-      { pattern: 'app/**/*.js', included: false },
-      { pattern: 'app/*.html', included: false },
-      { pattern: 'app/**/*.html', included: false },
-      { pattern: 'node_modules/*/*.json', included: false, watched: false },
-      { pattern: 'node_modules/systemjs-plugin-babel/*.js', included: false, watched: false },
-      { pattern: 'node_modules/systemjs-plugin-babel/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/systemjs-plugin-text/text.js', included: false, watched: false },
-      { pattern: 'node_modules/angular2/*.js', included: false, watched: false },
-      { pattern: 'node_modules/angular2/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/ui-router-ng2/*.js', included: false, watched: false },
-      { pattern: 'node_modules/ui-router-ng2/**/*.js', included: false, watched: false }
-    ],
+  // list of files / patterns to load in the browser
+  files: [
+    { pattern: 'node_modules/es6-shim/es6-shim.min.js', watched: false },
+    { pattern: 'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js', watched: false },
+    { pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', watched: false },
+    { pattern: 'app/*.js', included: false },
+    { pattern: 'app/**/*.js', included: false },
+    { pattern: 'app/*.html', included: false },
+    { pattern: 'app/**/*.html', included: false },
+    { pattern: 'node_modules/*/*.json', included: false, watched: false },
+    { pattern: 'node_modules/systemjs-plugin-babel/*.js', included: false, watched: false },
+    { pattern: 'node_modules/systemjs-plugin-babel/**/*.js', included: false, watched: false },
+    { pattern: 'node_modules/systemjs-plugin-text/text.js', included: false, watched: false },
+    { pattern: 'node_modules/angular2/*.js', included: false, watched: false },
+    { pattern: 'node_modules/angular2/**/*.js', included: false, watched: false },
+    { pattern: 'node_modules/rxjs/*.js', included: false, watched: false },
+    { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
+    { pattern: 'node_modules/ui-router-ng2/*.js', included: false, watched: false },
+    { pattern: 'node_modules/ui-router-ng2/**/*.js', included: false, watched: false }
+  ],
 
-    browsers: ['Chrome'],
+  browsers: ['Chrome'],
 
-    reporters: ['progress', 'systemjs-remap-coverage', 'coverage'],
-
-    coverageReporter: {
-      dir : 'coverage/',
-      reporters: [
-        {type: 'html'},
-        {type: 'json'}
-      ]
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
     }
-  });
+  },
+
+  reporters: ['progress', 'systemjs-remap-coverage', 'coverage'],
+
+  coverageReporter: {
+    dir : 'coverage/',
+    reporters: [
+      {type: 'html'},
+      {type: 'json'}
+    ]
+  }
+};
+
+if (process.env.TRAVIS) {
+  karmaConfig.browsers = ['Chrome_travis_ci'];
+}
+
+module.exports = function(config) {
+  config.set(karmaConfig);
 };
