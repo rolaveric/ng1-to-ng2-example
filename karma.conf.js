0cbfee28ababf387fa45df91084d53f4338d0199
rolaveric <rolaveric@gmail.com>
2016-05-01 20:02:10 +1000

    Added Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,54 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '.',
-    frameworks: ['systemjs', 'jasmine'],
-    systemjs: {
-      // Path to your SystemJS configuration file
-      configFile: 'app/config.js',
+var karmaConfig = {
+  basePath: '.',
+  frameworks: ['systemjs', 'jasmine'],
+  systemjs: {
+    // Path to your SystemJS configuration file
+    configFile: 'app/config.js',
 
-      // Files to be included before all others
-      includeFiles: [
-        'node_modules/es6-shim/es6-shim.min.js',
-        'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
-        'node_modules/angular2/bundles/angular2-polyfills.js'
-      ],
-
-      // Patterns for files that you want Karma to make available, but not loaded until a module requests them. eg. Third-party libraries.
-      serveFiles: [
-        'app/!(*spec).js',
-        'app/**/!(*spec).js',
-        'app/*.html',
-        'app/**/*.html',
-        'node_modules/systemjs-plugin-babel/*.js',
-        'node_modules/systemjs-plugin-babel/**/*.js',
-        'node_modules/systemjs-plugin-text/text.js',
-        'node_modules/angular2/*.js',
-        'node_modules/angular2/**/*.js',
-        'node_modules/rxjs/*.js',
-        'node_modules/rxjs/**/*.js',
-        'node_modules/ui-router-ng2/*.js',
-        'node_modules/ui-router-ng2/**/*.js'
-      ]
-    },
-    // Test files
-    files: [
-      'app/*.spec.js',
-      'app/**/*.spec.js'
+    // Files to be included before all others
+    includeFiles: [
+      'node_modules/es6-shim/es6-shim.min.js',
+      'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js'
     ],
 
-    browsers: ['Chrome']
-  });
+    // Patterns for files that you want Karma to make available, but not loaded until a module requests them. eg. Third-party libraries.
+    serveFiles: [
+      'app/!(*spec).js',
+      'app/**/!(*spec).js',
+      'app/*.html',
+      'app/**/*.html',
+      'node_modules/systemjs-plugin-babel/*.js',
+      'node_modules/systemjs-plugin-babel/**/*.js',
+      'node_modules/systemjs-plugin-text/text.js',
+      'node_modules/angular2/*.js',
+      'node_modules/angular2/**/*.js',
+      'node_modules/rxjs/*.js',
+      'node_modules/rxjs/**/*.js',
+      'node_modules/ui-router-ng2/*.js',
+      'node_modules/ui-router-ng2/**/*.js'
+    ]
+  },
+  // Test files
+  files: [
+    'app/*.spec.js',
+    'app/**/*.spec.js'
+  ],
+
+  browsers: ['Chrome'],
+
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  }
+};
+
+if (process.env.TRAVIS) {
+  karmaConfig.browsers = ['Chrome_travis_ci'];
+}
+
+module.exports = function(config) {
+  config.set(karmaConfig);
 };
